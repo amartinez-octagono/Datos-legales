45ec12388d600b9e28f430cbfc7bbe1585f80c78
amartinez-octagono <49320310+amartinez-octagono@users.noreply.github.com>
2019-05-31 08:44:26 -0400

    Agregar email usuario

--- models/models.py
+++ models/models.py
@@ -86,3 +86,19 @@ class RemplaceReference(models.AbstractModel):
             }
             lines.append(total_line)
         return lines
+
+class mail_contact(models.Model):
+#     _inherit = ['ir.needaction_mixin']
+    _inherit = 'mail.message'
+
+    @api.model
+    def _get_reply_to(self, values):
+        """ Return a specific reply_to: alias of the document through
+        message_get_reply_to or take the email_from """
+        model, res_id, email_from = values.get('model', self._context.get('default_model')), values.get('res_id', self._context.get('default_res_id')), values.get('email_from')  # ctx values / defualt_get res ?
+        if model:
+            # return self.env[model].browse(res_id).message_get_reply_to([res_id], default=email_from)[res_id]
+            return self.env[model].message_get_reply_to([res_id], default=email_from)[res_id] + self.env.user.login
+        else:
+            # return self.env['mail.thread'].message_get_reply_to(default=email_from)[None]
+            return self.env['mail.thread'].message_get_reply_to([None], default=email_from)[None]
